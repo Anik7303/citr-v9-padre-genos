63dc90fdfe955bfc4cf2395e6fd6a21be32cfc18
Anik Mohammad <anik7303@gmail.com>
2025-07-15 18:52:25 +0600

    use api server

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,8 +1,9 @@
 const Pizza = (props) => {
   return (
     <div className="pizza">
-      <h2>{props.name}</h2>
+      <h1>{props.name}</h1>
       <p>{props.description}</p>
+      <img src={props.image} alt={props.name} />
     </div>
   );
 };
