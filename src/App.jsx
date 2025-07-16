3dd36e270e731b211b733ae44a7e71482333ab81
Anik Mohammad <anik7303@gmail.com>
2025-07-16 12:57:35 +0600

    refactoring layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { createElement, StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
 import PizzaOfTheDay from "./PizzaOfTheDay";
 
 const App = () => {
   return (
-    <StrictMode>
-      <div>
-        <h1>Padre Gino's - Order Now</h1>
-        <Order />
-        <PizzaOfTheDay />
-      </div>
-    </StrictMode>
+    <div>
+      <h1 className="logo">Padre Gino's - Order Now</h1>
+      <Order />
+      <PizzaOfTheDay />
+    </div>
   );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(createElement(App));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
